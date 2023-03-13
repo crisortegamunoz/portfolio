6acbb7e06b84c5c858464a06a3a46b7b8a87b277
Cristian Ortega <crisortegamunoz@gmail.com>
2023-03-13 09:01:53 -0300

    COP-001 => Eliminacion de la sección de contacto. Avances en la sección de portafolio, se inicia la carga de los proyectos en los que he trabajado

---
+++ js/portfolio-data.js
@@ -0,0 +1,30 @@
+const array = [
+    {
+        id: 1,
+        projectName: 'Rick & Morty API',
+        crationDate: '20-09-2020',
+        urlDemo: 'https://rick-and-morty-api-desafio.web.app/',
+        urlRepo: 'https://github.com/crisortegamunoz/CursoAngularLaEscalab/tree/master/Clase%204/rickandmorty',
+        description: 'Este mini proyecto nace de uno de las tantas tareas dadas en el curso de Angular impartido por Escalab. De todos los tuve que desarrollar es mi favorito ya que, había que desarrollar un proyecto de libre pensamiento, utilizando Angular y consumiendo la API de “The Rick and Morty”. ¿Por qué me gustó? Las razones son simples. Una de ellas es que, por fin después de mucho tiempo, pude “jugar” con el diseño de un sitio web usando HTML y CSS. Busque y agregue distintos package para mejorar un poco el sitio, entre ellos, un paginador para los personajes de la serie y un swipe, que lo utilice como banner dentro de la página web. Por último, investigue y aplique Firebase hosting para dejar una demo del sitio, así cualquiera podría verlo sin la necesidad de descargar el código fuente desde mi GitHub.',
+        technologies: [
+            {
+                name: 'HTML'
+            },
+            {
+                name: 'CSS'
+            },
+            {
+                name: 'JavaScript'
+            },
+            {
+                name: 'TypeScript'
+            },
+            {
+                name: 'Angular'
+            },
+            {
+                name: 'Firebase'
+            }
+        ]
+    }
+]
